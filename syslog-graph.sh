f6ec9c2f9ed390dcee10046877ae4b80bfee0d91
Jeremy Bennett <jeremy.bennett@embecosm.com>
2017-06-06 09:45:59 +0100

    Improvements to syslog monitor.
    ChangeLog:
    
    	* syslog-graph.sh: Use temporary files in the temporary
    	directory, correctly filter out 1 Jan (when router was replaced),
    	look for just "WAN 1 is" for line failure, put ticks on outside,
    	change boundaries.

--- syslog-graph.sh
+++ syslog-graph.sh
@@ -38,12 +38,12 @@ datedays() {
 # remdir=/tmp/syslog-graph-dir-$$
 remdir=testing
 remhost=192.168.44
-# tmpf1=/tmp/syslog-graph-tmp-1-$$
-# tmpf2=/tmp/syslog-graph-tmp-2-$$
-# tmpf3=/tmp/syslog-graph-tmp-3-$$
-tmpf1=tmpf1
-tmpf2=tmpf2
-tmpf3=tmpf3
+tmpf1=/tmp/syslog-graph-tmp-1-$$
+tmpf2=/tmp/syslog-graph-tmp-2-$$
+tmpf3=/tmp/syslog-graph-tmp-3-$$
+#tmpf1=tmpf1
+#tmpf2=tmpf2
+#tmpf3=tmpf3
 
 # Mount the remote directory, unmounting first if already mounted
 fusermount -q -u ${remdir} || true
@@ -51,6 +51,7 @@ rm -rf ${remdir}
 mkdir ${remdir}
 rm -f ${tmpf1}
 rm -f ${tmpf2}
+rm -f ${tmpf3}
 
 if ! sshfs embadmin@${remhost}:/var/log ${remdir}
 then
@@ -65,19 +66,19 @@ done
 
 for f in ${remdir}/syslog.*.gz
 do
-    gunzip -c $f | grep '\(Shut Down from ADSL Phy\)\|SHOWTIME' | tac >> ${tmpf1}
+    gunzip -c $f | grep '\(WAN 1 is\)\|SHOWTIME' | tac >> ${tmpf1}
 done
 
 # Find the fields of interest, grepping out any data on Jan 1, which is change
 # over of new router.
-sed -n -e 's/\([[:alpha:]]\{3\}\) \([ [:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) .*Shut Down.*$/2017,\1,\2,\3,\4,\5,0,0,0,0/p' -e 's/\([[:alpha:]]\{3\}\) \([ [:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) .*SHOWTIME UpSpeed=\([[:digit:]]\+\) DownSpeed=\([[:digit:]]\+\) SNR=\([-[:digit:]]\+\) Atten=\([-[:digit:]]\+\).*/2017,\1,\2,\3,\4,\5,\6,\7,\8,\9/p' ${tmpf1} | sed -e 's/- /-0/' -e 's/Jan/01/' -e 's/Feb/02/' -e 's/Mar/03/' -e 's/Apr/04/' -e 's/May/05/' -e 's/Jun/06/' -e 's/Jul/07/' -e 's/Aug/08/' -e 's/Sep/09/' -e 's/Oct/10/' -e 's/Nov/11/' -e 's/Dec/12/' | grep -v '^2017-01-01' | tac > ${tmpf2}
+sed -n -e 's/\([[:alpha:]]\{3\}\) \([ [:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) .*WAN 1 is.*$/2017,\1,\2,\3,\4,\5,0,0,0,0/p' -e 's/\([[:alpha:]]\{3\}\) \([ [:digit:]]\{2\}\) \([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\):\([[:digit:]]\{2\}\) .*SHOWTIME UpSpeed=\([[:digit:]]\+\) DownSpeed=\([[:digit:]]\+\) SNR=\([-[:digit:]]\+\) Atten=\([-[:digit:]]\+\).*/2017,\1,\2,\3,\4,\5,\6,\7,\8,\9/p' ${tmpf1} | tac > ${tmpf2}
 
 # Break out the fields
 rm ${tmpf1}
 while IFS='' read -r line || [[ -n "${line}" ]]
 do
     year=$(echo ${line} | cut -d ',' -f 1)
-    mon=$(echo ${line} | cut -d ',' -f 2 | sed -e 's/^0//')
+    mon=$(echo ${line} | cut -d ',' -f 2)
     day=$(echo ${line} | cut -d ',' -f 3 | sed -e 's/^0//')
     hour=$(echo ${line} | cut -d ',' -f 4 | sed -e 's/^0//')
     min=$(echo ${line} | cut -d ',' -f 5 | sed -e 's/^0//')
@@ -87,17 +88,22 @@ do
     snr=$(echo ${line} | cut -d ',' -f 9)
     atten=$(echo ${line} | cut -d ',' -f 10)
 
-    printf "%04d-%02d-%02d/%02d:%02d:%02d\t%d\t%d\t%d\t%d\n" ${year} ${mon} ${day} ${hour} \
-           ${min} ${sec} ${down} ${up} ${snr} ${atten} >> ${tmpf1}
+    if [ \( ${mon} != "Jan" \) -o \( ${day} -ne 1 \) ]
+    then
+	printf "%04d-%3s-%02d/%02d:%02d:%02d\t%d\t%d\t%d\t%d\n" ${year} ${mon} \
+	       ${day} ${hour} ${min} ${sec} ${down} ${up} ${snr} ${atten} \
+	       >> ${tmpf1}
+    fi
 done < ${tmpf2}
 
 cat > ${tmpf3} <<EOF
 set xlabel "date"
-stats 'tmpf3' using 1 name 't' nooutput
 set xdata time
-set timefmt "%Y-%m-%d/%H:%M:%S"
+set timefmt "%Y-%b-%d/%H:%M:%S"
 set format x "%d-%b"
-set xrange ["2017-05-29/00:00:00" : "2017-06-06/00:00:00"]
+set xrange ["2017-May-30/00:00:00" : "2017-Jun-07/00:00:00"]
+set xtics out
+set ytics out
 set style line 1 lc rgb '#0060ad' lt 1 lw .1 pt 7 ps 1.5   # --- blue
 set style line 2 lc rgb '#ad0060' lt 1 lw .1 pt 7 ps 1.5   # --- red
 set style line 3 lc rgb '#60ad00' lt 1 lw .1 pt 7 ps 1.5   # --- green
